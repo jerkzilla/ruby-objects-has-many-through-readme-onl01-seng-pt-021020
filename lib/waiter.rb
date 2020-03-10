20164e8875cef4ed3aac46bca06cb9301418152c
John J Marshall Jr <johnjamesmarshalljr@gmail.com>
2020-03-10 21:15:34 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -17,8 +17,8 @@ class Waiter
     Meal.new(self, customer, total, tip)
   end
 
-  def waiters 
-    meals.map do |meal| meal.waiter 
+  def waiters
+    meals.map do |meal| meal.waiter
     end
   end
 
